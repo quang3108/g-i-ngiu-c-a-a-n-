b439b6862e93a2afe3233c73284d2607ce61aada
quang3108 <85300869+quang3108@users.noreply.github.com>
2021-07-02 16:29:02 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,17 +1,17 @@
 const CONFIG = {
-    introTitle: 'Babe à!',
-    introDesc: `tớ với cậu chơi với nhau cũng 
-    gần 9 năm rồi nhỉ`,
+    introTitle: 'bẹn vy yêu dấu',
+    introDesc: `Tao nói là tao
+    làm được mà :)))`,
     btnIntro: 'hihi',
-    title: 'tớ rất vui và tự hào vì có đc người bạn thân như cậu',
-    desc: 'trước tiên là nhớ trả nợ đó :>>>',
+    title: 'nhớ cho t lên ké story :)) ',
+    desc: 'nhớ che tên đi nhaaa:>>>',
     btnYes: 'ok. tất nhiên rồi :)))',
     btnNo: 'Không, có cái qq :3',
-    question:'hứa là phải liên lạc thường xuyên nha có mess với zalo rồi đó',
-    btnReply: 'Gửi cho tớ <3',
-    reply: 'biết rồi <33333333',
-    mess: 'tớ biết mà 🥰. nhớ giữ gìn sức khỏe đó',
-    messDesc: 'sau này về nhớ dẫn tớ đi ăn.',
+    question:'thấy t đỉnh không :>>>',
+    btnReply: 'Gửi cho t <3',
+    reply: 'đỉnh lắm luôn <33333333',
+    mess: 't biết mà khen hoài :)))',
+    messDesc: 'sau này về nhớ bao t đi ăn.',
     btnAccept: 'Okiiiii lun <3',
     messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
